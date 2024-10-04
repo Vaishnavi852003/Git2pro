eb0465c225687c7d6bc36737e537c7174c62786a
Vaishnavi <vaishnavipatil852003@gmail.com>
2024-10-04 21:02:10 +0530

    my sec commit

---
+++ Git1.java
@@ -0,0 +1,11 @@
+public class Git1{
+   public static void main(String args [])
+{
+   System.out.println("Hello Git !!!");
+   System.out.println("Hello Git !!!");
+   System.out.println("helllloo");
+   int a=20;
+   int b= 30;
+   System.out.println(a+b);
+}
+

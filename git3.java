e4bbd040a9b664361c9f160a91d3556205ccfc29
Vaishnavi <110183854+Vaishnavi852003@users.noreply.github.com>
2024-10-04 21:46:49 +0530

    git3.java

--- git3.java
+++ git3.java
@@ -2,11 +2,8 @@ public class First{
    public static void main(String args [])
 {
    System.out.println("Hello Git !!!");
-<<<<<<< HEAD
-  
-=======
    System.out.println("Hello Git !!!");
    System.out.println("helllloo");
->>>>>>> 5720e464cf8f4efa2b1a67dee3720a491f58ab6b
+}
 }
 
